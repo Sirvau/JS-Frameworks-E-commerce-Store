5065ddcdf80abc488fe33e52e59979d635b28aea
Sirvau <sirols50984@stud.noroff.no>
2024-09-21 13:47:47 +0200

    Made route-path component and imported it to app component

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,39 +1,13 @@
 
-import { Route, Routes } from 'react-router-dom'
-import Layout from '../components/layout'
 
-
-
-import '../styles/index.css'
-import HomePage from './home'
-import ContactPage from './contact'
-import CheckoutSuccessPage from './checkout-success'
-import CheckoutPage from './checkout'
-import IndividualProductPage from './individual-product'
-import RouteNotFound from './route-not-found'
-
-
+import RoutePaths from "../routes"
 
 
 function App() {
   
-
-
   return (
     <div>
-  <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="contact" element={<ContactPage />} />
-          <Route path="individual-product" element={<IndividualProductPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-          <Route path="checkout-success" element={<CheckoutSuccessPage />} />
-          <Route path="*" element={<RouteNotFound />} />
-    
-        </Route>
-      </Routes>
-   
-
+    <RoutePaths/>
   </div>
   )
 }
