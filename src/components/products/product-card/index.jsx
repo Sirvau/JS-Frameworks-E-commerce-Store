2047ab732def6c4b9e45ad813fc10f980beac2bf
Sirvau <sirols50984@stud.noroff.no>
2024-09-24 11:19:25 +0200

    Made conditionally rendering of prices with or without discount

--- src/components/products/product-card/index.jsx
+++ src/components/products/product-card/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function ProductCard({ product }) {
- 
-  const discountPercentage = ((product.price - product.discountedPrice) / product.price) * 100;
+  const hasDiscount = product.discountedPrice < product.price;
+  const discountPercentage = hasDiscount
+    ? ((product.price - product.discountedPrice) / product.price) * 100
+    : 0;
 
   return (
     <li className="mx-4">
@@ -15,12 +17,20 @@ function ProductCard({ product }) {
         <h2 className="ps-4 pt-4">{product.title}</h2>
         <div className="flex flex-row justify-between mt-2">
           <div className="py-2 ps-4">
-            <p>{product.discountedPrice} kr.</p>
-            <p className="line-through">{product.price} kr.</p>
+            {hasDiscount ? (
+              <>
+                <p>{product.discountedPrice} kr.</p>
+                <p className="line-through">{product.price} kr.</p>
+              </>
+            ) : (
+              <p>{product.price} kr.</p>
+            )}
           </div>
-          <p className="bg-md-beige p-2 self-center">
-            {discountPercentage.toFixed()}% OFF
-          </p>
+          {hasDiscount && (
+            <p className="bg-md-beige p-2 self-center">
+              {discountPercentage.toFixed()}% OFF
+            </p>
+          )}
         </div>
         <button className="bg-dark-brown text-light-beige px-5 py-4 mx-auto my-6">
           View Product
