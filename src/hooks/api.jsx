36415c1fc9ce2f1aa132221791bb649791ed615c
Sirvau <sirols50984@stud.noroff.no>
2024-09-25 13:53:30 +0200

    Updated useApi hook to handle both product lists and individual product.

--- src/hooks/api.jsx
+++ src/hooks/api.jsx
@@ -1,29 +1,29 @@
 import { useState, useEffect } from 'react';
 
-function useApi (url) {
-    const [products, setProducts] = useState([]); 
-    const [isLoading, setIsLoading] = useState(false); 
-    const [isError, setIsError] = useState(false); 
-  
-    useEffect(() => {
-      async function getData() {
-        try {
-          setIsError(false); 
-          setIsLoading(true); 
-          const response = await fetch(url);
-          const json = await response.json();
-          console.log(json); 
-          setProducts(json.data);
-          setIsLoading(false); 
-        } catch (error) {
-          setIsLoading(false);
-          setIsError(true); 
-        }
+function useApi(url) {
+  const [data, setData] = useState(null); 
+  const [isLoading, setIsLoading] = useState(false); 
+  const [isError, setIsError] = useState(false); 
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsError(false); 
+        setIsLoading(true); 
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json.data); 
+        setIsLoading(false); 
+      } catch (error) {
+        setIsLoading(false);
+        setIsError(true); 
       }
-  
-      getData();
-    }, [url]);
-    return { products, isLoading, isError };
+    }
+
+    getData();
+  }, [url]);
+
+  return { data, isLoading, isError };
 }
 
-export default useApi
+export default useApi;
