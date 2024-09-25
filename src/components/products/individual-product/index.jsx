ea646cf87f898f9db258b3e273ece2864bfeda67
Sirvau <sirols50984@stud.noroff.no>
2024-09-25 20:35:20 +0200

    Styling and responsiveness on page

--- src/components/products/individual-product/index.jsx
+++ src/components/products/individual-product/index.jsx
@@ -27,34 +27,51 @@ function IndividualProduct() {
     : 0;
 
   return (
-    <div>
-      <div>
-      <h2 className="pt-4 uppercase text-center text-lg md:text-xl font-semibold">
+    <div className="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center mt-20 md:mx-10 lg:mx-16 xl:mx-20  shadow-md bg-light-beige ">
+      <div className="w-80 ">
+      <img className="h-[320px] sm:h-[380px] w-full object-cover" src={product.image.url} alt={product.image.alt || product.title} />
+      {hasDiscount && (
+            <p className="bg-dark-brown text-light-beige p-2 self-center text-center tracking-wide">
+              {discountPercentage.toFixed()} % OFF
+            </p>
+          )}
+      </div>
+      <div className="mt-6 sm:flex sm:flex-col  sm:justify-center mx-6 sm:mx-8 md:mx-12 sm:mt-28">
+        <h2 className="uppercase text-2xl  tracking-wide ">
         {product.title}
-      </h2>
-      <img src={product.image.url} alt={product.image.alt || product.title} />
-      <p>{product.description}</p>
-      <p>Price: ${product.discountedPrice}</p>
-      <p>Rating: {product.rating}</p>
-      <p>Tags: {product.tags}</p>
-      <div className="py-2 ps-4">
-            {hasDiscount ? (
+        </h2>
+     
+       
+        <p className="font-medium mt-2 sm:mt-6 ">{product.description}</p>
+        <hr></hr>
+        <div className="flex justify-around mt-6 sm:mt-12">
+        {hasDiscount ? (
               <>
                 <p className="font-semibold">{product.discountedPrice} kr.</p>
-                <p className="line-through font-normal">{product.price} kr.</p>
+                <p className="line-through font-medium">{product.price} kr.</p>
               </>
             ) : (
               <p>{product.price} kr.</p>
             )}
-          </div>
-          {hasDiscount && (
-            <p className="bg-md-beige p-2 self-center">
-              {discountPercentage.toFixed()}% OFF
-            </p>
-          )}
-    </div>
-    </div>
+        </div>
+        <button className="bg-oak-brown text-light-beige font-semibold px-28 sm:px-20 py-3 mx-auto my-6 mt-6 sm:mt-12 rounded-xl flex justify-self-center ">Add to bag</button>
+      </div>
+      </div>
+  
+       
+   
   );
 }
 
 export default IndividualProduct;
+
+function Reviews() {
+
+  return (
+    <div>
+      <p>{product.reviews}</p>
+    </div>
+  );
+ 
+}
+
