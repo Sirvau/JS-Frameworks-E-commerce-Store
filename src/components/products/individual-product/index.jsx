7d3cf51c1f8da4a39841a513faf2c567e4ef86d6
Sirvau <sirols50984@stud.noroff.no>
2024-10-01 23:38:44 +0200

    Adding oneclick event to button, adding product to cart. Styling, and destructuring of object

--- src/components/products/individual-product/index.jsx
+++ src/components/products/individual-product/index.jsx
@@ -1,15 +1,15 @@
 import useApi from '../../../hooks/api';
 import { useParams } from 'react-router-dom';
 import ReviewList from '../product-reviews/review-list';
-
-
-
+import { useCart } from '../../../context/cart-context'; 
 
 function IndividualProduct() {
   let { id } = useParams();
   const { data: product, isLoading, isError } = useApi(
-    `https://v2.api.noroff.dev/online-shop/${id}`,
+    `https://v2.api.noroff.dev/online-shop/${id}`
   );
+
+  const { addToCart } = useCart(); 
 
   if (isLoading) {
     return <div>Loading product...</div>;
@@ -23,60 +23,56 @@ function IndividualProduct() {
     return <div>Product not found</div>;
   }
 
-  const hasDiscount = product.discountedPrice < product.price;
-  const discountPercentage = hasDiscount
-    ? ((product.price - product.discountedPrice) / product.price) * 100
-    : 0;
+  const { title, image, description, price, discountedPrice, reviews } = product;
+
+ 
+  const hasDiscount = discountedPrice < price;
+  const discountPercentage = hasDiscount ? ((price - discountedPrice) / price) * 100 : 0;
+
+  
+  const handleAddToCart = () => {
+    addToCart({ ...product, quantity: 1 }); 
+  };
 
   return (
     <div>
-    <div className="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center mt-20 md:mx-10 lg:mx-16 xl:mx-20  shadow-md bg-light-beige ">
-    
-      <div className="w-80 ">
-      <img className="h-[320px] sm:h-[380px] w-full object-cover" src={product.image.url} alt={product.image.alt || product.title} />
-      {hasDiscount && (
+      <div className="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center mt-20 md:mx-10 lg:mx-16 xl:mx-20 shadow-md bg-light-beige ">
+        <div className="w-80 ">
+          <img className="h-[320px] sm:h-[380px] w-full object-cover brightness-90" src={image.url} alt={image.alt || title} />
+          {hasDiscount && (
             <p className="bg-dark-brown text-light-beige p-2 self-center text-center tracking-wide">
-              {discountPercentage.toFixed()} % OFF
+              {discountPercentage.toFixed()}% OFF
             </p>
           )}
-      </div>
-      <div className="mt-6 sm:flex sm:flex-col  sm:justify-center mx-6 sm:mx-8 md:mx-12 sm:mt-28">
-        <h2 className="uppercase text-2xl  tracking-wide ">
-        {product.title}
-        </h2>
-     
-       
-        <p className="font-medium mt-2 sm:mt-6 ">{product.description}</p>
-   
-        <div className="flex justify-around mt-6 sm:mt-12">
-        {hasDiscount ? (
+        </div>
+        <div className="mt-6 sm:flex sm:flex-col sm:justify-center mx-6 sm:mx-8 md:mx-12 sm:mt-28">
+          <h2 className="uppercase text-2xl tracking-wide ">{title}</h2>
+          <p className="font-medium mt-2 sm:mt-6 ">{description}</p>
+          <div className="flex justify-around mt-6 sm:mt-12">
+            {hasDiscount ? (
               <>
-                <p className="font-semibold">{product.discountedPrice} kr.</p>
-                <p className="line-through font-medium">{product.price} kr.</p>
+                <p className="font-semibold">{discountedPrice} kr.</p>
+                <p className="line-through font-medium">{price} kr.</p>
               </>
             ) : (
-              <p>{product.price} kr.</p>
+              <p>{price} kr.</p>
             )}
+          </div>
+          <button
+            className="bg-oak-brown text-light-beige font-semibold px-28 sm:px-20 py-3 mx-auto my-6 mt-6 sm:mt-12 rounded-xl flex justify-self-center"
+            onClick={handleAddToCart} 
+          >
+            Add to bag
+          </button>
         </div>
-        <button className="bg-oak-brown text-light-beige font-semibold px-28 sm:px-20 py-3 mx-auto my-6 mt-6 sm:mt-12 rounded-xl flex justify-self-center ">Add to bag</button>
-      </div>
       </div>
       <section className="mt-32 py-6 bg-md-beige">
         <h1 className="uppercase text-2xl ps-6">Reviews</h1>
-        <hr className="my-4 mx-6 border-dark-brown"></hr>
-        <div className="mx-6"> <ReviewList reviews={product.reviews} /></div>
-       
-      
+        <hr className="my-4 mx-6 border-dark-brown" />
+        <div className="mx-6"><ReviewList reviews={reviews} /></div>
       </section>
-    
-      
-      </div>
-       
-   
+    </div>
   );
 }
 
 export default IndividualProduct;
-
-
-
