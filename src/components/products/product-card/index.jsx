a2c2845e85fe03dbc4cf4b70e79124707d6864b1
Sirvau <sirols50984@stud.noroff.no>
2024-10-02 09:07:10 +0200

    Destructuring of product object. Changed React version

--- src/components/products/product-card/index.jsx
+++ src/components/products/product-card/index.jsx
@@ -3,31 +3,35 @@ import { Link } from 'react-router-dom';
 
 
 function ProductCard({ product }) {
-  const hasDiscount = product.discountedPrice < product.price;
+
+  const {id, title, image, price, discountedPrice} = product;
+
+  {/*Product Discount Calculation**/}
+  const hasDiscount = discountedPrice < price;
   const discountPercentage = hasDiscount
-    ? ((product.price - product.discountedPrice) / product.price) * 100
+    ? ((price - discountedPrice) / price) * 100
     : 0;
 
   return (
     <li className="mx-4 tracking-wide ">
-           <Link to={`/individual-product/${product.id}`}>
-      <div className="my-4 sm:mx-6 flex flex-col  shadow-lg w-80 sm:w-70 md:w-60 bg-light-beige">
+           <Link to={`/individual-product/${id}`}>
+      <div className="flex flex-col w-80  sm:w-60 md:w-50  my-4 sm:mx-4 shadow-lg  bg-light-beige">
  
         <img
-          className="brightness-90 h-[250px] w-full object-cover sm:h-[200px]  px-4 pt-4"
-          src={product.image.url}
-          alt={product.image.alt || product.title}
+          className="brightness-90 h-[250px] sm:h-[200px] w-full object-cover   px-4 pt-4"
+          src={image.url}
+          alt={image.alt || title}
         />
-        <h2 className=" pt-4 uppercase text-center text-lg md:text-xl font-semibold ">{product.title}</h2>
+        <h2 className=" pt-4 uppercase text-center text-lg md:text-xl font-bold ">{title}</h2>
         <div className="flex flex-row justify-between mt-2">
           <div className="py-2 ps-4">
             {hasDiscount ? (
               <>
-                <p className="font-semibold">{product.discountedPrice} kr.</p>
-                <p className="line-through font-medium">{product.price} kr.</p>
+                <p className="font-semibold">{discountedPrice} kr.</p>
+                <p className="line-through font-medium">{price} kr.</p>
               </>
             ) : (
-              <p>{product.price} kr.</p>
+              <p>{price} kr.</p>
             )}
           </div>
           {hasDiscount && (
