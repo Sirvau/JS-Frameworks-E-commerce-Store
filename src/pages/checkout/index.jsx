6620ce95d1c76a53f80546bdd2e60a8be8c1fe0d
Sirvau <sirols50984@stud.noroff.no>
2024-10-02 22:13:13 +0200

    border opacity adjustment

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -3,9 +3,9 @@ import CartList from "../../components/cart-list"
 
 function CheckoutPage() {
     return ( 
-    <div>
+    <div class="container lg:w-3/4 xl:w-2/3">
      <h1 className="uppercase text-2xl mx-4  tracking-wide">Your shoppingbag</h1>    
-     <hr className="my-4 border-dark-brown"></hr>
+     <hr className="my-4 border-dark-brown opacity-80"></hr>
       <div><CartList/></div> 
         </div>
     )
