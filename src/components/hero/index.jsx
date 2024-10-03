d5bccf5b5816450a09b742b3a5c1ad16f0be0556
Sirvau <sirols50984@stud.noroff.no>
2024-10-03 12:22:12 +0200

    Added background image to hero texting. Added text. Customized styling and responsiveness

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,17 @@
+import heroBackground from '../../../public/assets/images/hero-background.jpg';
+
+function HeroSection() {
+    return (
+        <div
+            className="relative h-[280px] md:h-[340px] lg:h-[400px] xl:h-[460px] w-full bg-cover bg-center mb-12"
+            style={{ backgroundImage: `url(${heroBackground})` }}
+        >
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-light-beige bg-dark-brown opacity-40">
+                <p className="text-2xl md:text-3xl lg:text-4xl font-bold">Find it. Love it.</p>
+                <p className="text-7xl md:text-8xl lg:text-9xl mt-2">Shop it!</p>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
