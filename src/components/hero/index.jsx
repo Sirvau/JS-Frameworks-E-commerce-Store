4651ee4bacc11475a0b2caca9dc69370a582dc4d
Sirvau <sirols50984@stud.noroff.no>
2024-10-03 13:27:13 +0200

    Opacity adjustment

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -6,7 +6,7 @@ function HeroSection() {
             className="relative h-[280px] md:h-[340px] lg:h-[400px] xl:h-[460px] w-full bg-cover bg-center mb-12"
             style={{ backgroundImage: `url(${heroBackground})` }}
         >
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-light-beige bg-dark-brown opacity-40">
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-light-beige bg-dark-brown opacity-20">
                 <p className="text-2xl md:text-3xl lg:text-4xl font-bold">Find it. Love it.</p>
                 <p className="text-7xl md:text-8xl lg:text-9xl mt-2">Shop it!</p>
             </div>
