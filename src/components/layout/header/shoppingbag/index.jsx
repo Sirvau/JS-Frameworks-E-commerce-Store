06670e972f7032346a7782c94be20dd5ec45bcee
Sirvau <sirols50984@stud.noroff.no>
2024-10-04 10:49:27 +0200

    header icon adjustments

--- src/components/layout/header/shoppingbag/index.jsx
+++ src/components/layout/header/shoppingbag/index.jsx
@@ -11,7 +11,7 @@ function ShoppingBagIcon() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="size-8"
+      className="size-7"
     >
       <path
         strokeLinecap="round"
@@ -19,11 +19,11 @@ function ShoppingBagIcon() {
         d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
       />
     </svg>
-    <div className="absolute -top-3 -right-3 sm:-right-0 bg-dark-brown text-white rounded-full size-6 flex items-center justify-center text-xs tracking-widest">
+    <div className="absolute -top-5 -right-3 sm:-right-0 font-semibold bg-oak-brown text-light-beige rounded-full size-6 flex items-center justify-center text-xs tracking-widest">
       {totalItems}
     </div>
 
-  <span className="hidden sm:block">Shopping Bag</span></div>
+  <span className="hidden sm:block font-medium">Shopping Bag</span></div>
     
   }
 
